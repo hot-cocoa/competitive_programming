a14612d7a4eaacd90e79a65c21342710eca6d5cf
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2019-05-25 23:59:40 +0900

    add minimum spanning tree (prim algorithm) in graph

---
+++ library/graph/minimum_spanning_tree.cpp
@@ -0,0 +1,37 @@
+struct edge {
+    int to, cost;
+    edge(int to, int cost) : to{to}, cost{cost} {}
+};
+
+using vec = std::vector<edge>;
+using Graph = std::vector<vec>;
+
+class MinimumSpanningTree {
+private:
+    using pii = std::pair<int, int>;
+
+public:
+    int prim(const Graph &g)
+    {
+        std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq;
+        pq.emplace(0, 0);
+
+        std::vector<bool> visited(g.size());
+        int total_cost = 0;
+        while (!pq.empty()) {
+            pii p = pq.top(); pq.pop();
+
+            int curr = p.second;
+            if (visited[curr])
+                continue;
+
+            visited[curr] = true;
+            total_cost += p.first;
+
+            for (edge e : g[curr])
+                pq.emplace(e.cost, e.to);
+        }
+
+        return total_cost;
+    }
+};
