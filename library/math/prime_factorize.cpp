0d84ee61fa441cd16f35158ceea0191837713750
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2019-06-15 16:59:28 +0900

    add prime factorize in math

---
+++ library/math/prime_factorize.cpp
@@ -0,0 +1,15 @@
+std::map<int, int> prime_factorize(int n)
+{
+    std::map<int, int> res;
+    for (int i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            ++res[i];
+            n /= i;
+        }
+    }
+
+    if (n != 1)
+        res[n] = 1;
+
+    return res;
+}
