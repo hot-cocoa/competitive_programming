c08ab7218386b116c4f436f99052dd86d81bf965
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2020-02-02 19:44:38 +0900

    add ModInt in util

---
+++ library/util/ModInt.cpp
@@ -0,0 +1,87 @@
+/**
+ * TODO: verifyすること！
+ */
+
+using int64 = long long;
+
+template <int64 MOD>
+class ModInt {
+private:
+    int64 v;
+
+    int64 mod_of(int64 x)
+    {
+        if (x < 0)
+            x += MOD;
+
+        return x % MOD;
+    }
+
+    int64 mod_pow(int64 x, int64 n)
+    {
+        if (n == 0)
+            return 1;
+
+        int64 res = mod_pow(x * x % MOD, n / 2);
+        if (n & 1)
+            res = res * x % MOD;
+
+        return res;
+    }
+
+public:
+    ModInt(int64 x = 0) : v(mod_of(x)) {}
+
+    ModInt operator + (const ModInt &rhs) const
+    {
+        return ModInt(v + rhs.v);
+    }
+    ModInt operator - (const ModInt &rhs) const
+    {
+        return ModInt(v - rhs.v);
+    }
+    ModInt operator * (const ModInt &rhs) const
+    {
+        return ModInt(v * rhs.v);
+    }
+    ModInt operator / (const ModInt &rhs) const
+    {
+        return ModInt(v * mod_pow(rhs.v, MOD - 2));
+    }
+    ModInt operator += (const ModInt &rhs)
+    {
+        v = mod_of(v + rhs.v);
+        return *this;
+    }
+    ModInt operator -= (const ModInt &rhs)
+    {
+        v = mod_of(v - rhs.v);
+        return *this;
+    }
+    ModInt operator *= (const ModInt &rhs)
+    {
+        v = mod_of(v * rhs.v);
+        return *this;
+    }
+    ModInt operator /= (const ModInt &rhs)
+    {
+        v = mod_of(v * mod_pow(rhs.v, MOD - 2));
+        return *this;
+    }
+    ModInt operator - () const
+    {
+        return ModInt(-v);
+    }
+    bool operator == (const ModInt &rhs) const
+    {
+        return v == rhs.v;
+    }
+    bool operator != (const ModInt &rhs) const
+    {
+        return v != rhs.v;
+    }
+    int64 value()
+    {
+        return v;
+    }
+};
