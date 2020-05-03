ff2723645e24568bae4ae5f4c8fdf3028f8f8a45
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2020-05-03 20:19:16 +0900

    add combination in math

---
+++ library/math/combination.cpp
@@ -0,0 +1,27 @@
+using int64 = long long;
+
+class Combination {
+private:
+    constexpr static int64 DEFAULT = -1;
+    std::vector<std::vector<int64>> c;
+public:
+    Combination(int max)
+    {
+        c.assign(max + 1, std::vector<int64>(max + 1, DEFAULT));
+    }
+
+    // calc nCr
+    int64 calc(int n, int r)
+    {
+        if (n < r)
+            return 0LL;
+
+        if (n == r || r == 0)
+            return 1LL;
+
+        if (c[n][r] != DEFAULT)
+            return c[n][r];
+
+        return c[n][r] = calc(n - 1, r - 1) + calc(n - 1, r);
+    }
+};
