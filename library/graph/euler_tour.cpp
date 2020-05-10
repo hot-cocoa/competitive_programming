3b09de15cf7b3bd8401088aaacf098b22338fd1c
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2020-05-10 19:38:36 +0900

    add euler tour in graph

---
+++ library/graph/euler_tour.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+
+using Graph = std::vector<std::vector<int>>;
+
+class EulerTour {
+private:
+    Graph g;
+    std::vector<int> l, r;
+
+    void dfs(int v, int p, int &k)
+    {
+        l[v] = k++;
+        for (int to : g[v])
+            if (to != p)
+                dfs(to, v, k);
+
+        r[v] = k;
+    }
+
+    void init(int V)
+    {
+        l.resize(V);
+        r.resize(V);
+    }
+
+    void build()
+    {
+        int k = 0;
+        dfs(0, -1, k);
+    }
+
+public:
+    EulerTour(Graph g) : g{g} {
+        init(g.size());
+        build();
+    }
+
+    int left(int v)
+    {
+        return l[v];
+    }
+
+    int right(int v)
+    {
+        return r[v];
+    }
+};
