e1e3a0a4b1ce0cd12e29c6593ba6b82cf9e647e5
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2020-06-05 22:16:35 +0900

    update lazy segment tree

--- library/data_structure/lazy_segment_tree.cpp
+++ library/data_structure/lazy_segment_tree.cpp
@@ -7,10 +7,12 @@ private:
     using FT = std::function<T(T, T)>;
     using FX = std::function<T(T, U)>;
     using FU = std::function<U(U, U)>;
+    using FS = std::function<U(U, int)>;
 
     const FT ft;
     const FX fx;
     const FU fu;
+    const FS fs;
     const T et;
     const U eu;
 
@@ -28,7 +30,7 @@ private:
         lazy.assign(2 * N - 1, eu);
     }
 
-    void eval(int k)
+    void eval(int k, int len)
     {
         if (lazy[k] == eu)
             return;
@@ -37,23 +39,16 @@ private:
             lazy[k * 2 + 1] = fu(lazy[k * 2 + 1], lazy[k]);
             lazy[k * 2 + 2] = fu(lazy[k * 2 + 2], lazy[k]);
         }
-        dat[k] = fx(dat[k], lazy[k]);
+        dat[k] = fx(dat[k], fs(lazy[k], len));
         lazy[k] = eu;
-    }
-
-public:
-    LazySegmentTree(int N, const FT &ft, const FX &fx, const FU &fu, const T &et, const U &eu) :
-        ft{ft}, fx{fx}, fu{fu}, et{et}, eu{eu}
-    {
-        build(N);
     }
 
     void update(int a, int b, U x, int k, int l, int r)
     {
-        eval(k);
+        eval(k, r - l);
         if (a <= l && r <= b) {
             lazy[k] = fu(lazy[k], x);
-            eval(k);
+            eval(k, r - l);
         }
         else if (a < r && l < b) {
             update(a, b, x, k * 2 + 1, l, (l + r) / 2);
@@ -62,14 +57,9 @@ public:
         }
     }
 
-    void update(int l, int r, U x)
-    {
-        update(l, r + 1, x, 0, 0, N);
-    }
-
     T query(int a, int b, int k, int l, int r)
     {
-        eval(k);
+        eval(k, r - l);
         if (r <= a || b <= l)
             return et;
 
@@ -79,6 +69,27 @@ public:
         T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
         T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
         return ft(vl, vr);
+    }
+public:
+    LazySegmentTree(
+        int N, const FT &ft, const FX &fx, const FU &fu, const FS &fs,
+        const T &et, const U &eu) : ft{ft}, fx{fx}, fu{fu}, fs{fs}, et{et}, eu{eu}
+    {
+        build(N);
+    }
+
+    void assign(int n, int x)
+    {
+        for (int i = 0; i < n; i++)
+            dat[i + N - 1] = x;
+
+        for (int i = N - 2; i >= 0; i--)
+            dat[i] = ft(dat[i * 2 + 1], dat[i * 2 + 2]);
+    }
+
+    void update(int l, int r, U x)
+    {
+        update(l, r + 1, x, 0, 0, N);
     }
 
     T query(int l, int r)
